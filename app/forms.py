5e91fafadb1979e122fba1b63820810ae8e22246
Annie Lydens <aelydens@gmail.com>
2018-07-02 15:24:49 -0500

    Get admin login/logout working

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 class EncryptForm(FlaskForm):
     message = StringField('Message', validators=[DataRequired()])
     submit = SubmitField('Encrypt Me!')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,19 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import EncryptForm
+from app.forms import EncryptForm, LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/admin')
+@login_required
+def admin():
+    return render_template('admin.html')
 
 @app.route('/encrypt', methods=['GET', 'POST'])
 def encrypt():
@@ -15,3 +23,22 @@ def encrypt():
         return redirect(url_for('index'))
 
     return render_template('encrypt.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid login credentials')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('admin'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
